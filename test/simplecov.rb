958d52f6fb3d6b24a38c4b23553264734c8282d6
Rick Hull <rick.hull@gmail.com>
2017-01-27 02:53:47 -0800

    revise text formatter

--- test/simplecov.rb
+++ test/simplecov.rb
@@ -6,15 +6,25 @@ class SimpleCov::Formatter::TextFormatter
   def format(result)
     tot = result.files
     rpt = ["Coverage: %0.1f%%" % tot.covered_percent,
-           "Coverage strength: %0.2f" % tot.covered_strength,
-           "Lines Covered: %i / %i" % [tot.covered_lines, tot.lines_of_code],
-           "Missed, Never, Skipped: %i, %i, %i" % [tot.missed_lines,
-                                                   tot.never_lines,
-                                                   tot.skipped_lines],
-           result.files.map { |sfile|
-             "%i%%\t%s" % [sfile.covered_percent, sfile.filename]
-           }.join("\n"),
-          ].join("\n")
+           "Strength: %0.2f" % tot.covered_strength,
+           "   Lines: %i" % tot.lines_of_code,
+           " Covered: %i" % tot.covered_lines,
+           "     N/A: %i" % tot.never_lines,
+          ]
+    if tot.missed_lines > 0
+      rpt << "Missed: %i" % tot.missed_lines
+    end
+    if tot.skipped_lines > 0
+      rpt << "Skipped: %i" % tot.skipped_lines
+    end
+    rpt << result.files.map { |sfile|
+      "%i%% (%i/%i)\t%s" % [sfile.covered_percent,
+                            sfile.covered_lines.length,
+                            sfile.lines_of_code,
+                            sfile.filename]
+    }.join("\n")
+    rpt = rpt.join("\n")
+
     puts
     puts rpt
     if File.writable?(FILENAME)
