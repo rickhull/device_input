8f7969196f43f944bb0b01820864b1bc4f5b3a45
Rick Hull <rick.hull@gmail.com>
2022-05-02 19:23:02 -0400

    add support for nix flakes with direnv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Provides a shell with useful gems activated";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [
+          (pkgs.ruby.withPackages (p: with p; [
+            rake
+            minitest
+	    slop
+	    simplecov
+          ]))
+        ];
+      };
+    });
+}
