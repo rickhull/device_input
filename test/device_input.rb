0b396efe06859c95db3a94f3b2d2b382d0f9db50
Rick Hull <rick.hull@gmail.com>
2022-05-02 19:21:36 -0400

    use expect() wrappers for MiniTest 6

--- test/device_input.rb
+++ test/device_input.rb
@@ -8,42 +8,42 @@ describe DeviceInput::Event do
   describe "type_labels" do
     it "must return an array of strings" do
       dne = E.type_labels(:does_not_exist)
-      dne.must_be_instance_of(Array)
-      dne.first.must_be_instance_of(String)
-      dne.last.must_be_instance_of(String)
+      expect(dne).must_be_instance_of(Array)
+      expect(dne.first).must_be_instance_of(String)
+      expect(dne.last).must_be_instance_of(String)
 
       # 0 is EV_SYN
       de = E.type_labels(0)
-      de.must_be_instance_of(Array)
-      de.first.must_be_instance_of(String)
-      de.last.must_be_instance_of(String)
+      expect(de).must_be_instance_of(Array)
+      expect(de.first).must_be_instance_of(String)
+      expect(de.last).must_be_instance_of(String)
     end
   end
 
   describe "code_labels" do
     it "must return an array of strings" do
       dne = E.code_labels(:does_not_exist, nil)
-      dne.must_be_instance_of(Array)
-      dne.first.must_be_instance_of(String)
-      dne.last.must_be_instance_of(String)
+      expect(dne).must_be_instance_of(Array)
+      expect(dne.first).must_be_instance_of(String)
+      expect(dne.last).must_be_instance_of(String)
 
       # 0,0 = EV_SYN,SYN_REPORT
       de = E.code_labels(0, 0)
-      de.must_be_instance_of(Array)
-      de.first.must_be_instance_of(String)
-      de.last.must_be_instance_of(String)
+      expect(de).must_be_instance_of(Array)
+      expect(de.first).must_be_instance_of(String)
+      expect(de.last).must_be_instance_of(String)
     end
   end
 
   describe "encode" do
     it "must return a string" do
-      E.encode(E::NULL_DATA).must_be_instance_of(String)
+      expect(E.encode(E::NULL_DATA)).must_be_instance_of(String)
     end
   end
 
   describe "decode" do
     it "must return a Data (struct)" do
-      E.decode(E::NULL_MSG).must_be_instance_of(E::Data)
+      expect(E.decode(E::NULL_MSG)).must_be_instance_of(E::Data)
     end
   end
 
@@ -53,28 +53,28 @@ describe DeviceInput::Event do
     end
 
     it "must have data" do
-      @event.data.must_be_instance_of(E::Data)
+      expect(@event.data).must_be_instance_of(E::Data)
     end
 
     it "must have a timestamp" do
-      @event.time.must_be_instance_of(Time)
+      expect(@event.time).must_be_instance_of(Time)
     end
 
     it "must have a type" do
-      @event.type.must_be_instance_of(String)
+      expect(@event.type).must_be_instance_of(String)
     end
 
     it "must have a code" do
-      @event.code.must_be_instance_of(String)
+      expect(@event.code).must_be_instance_of(String)
     end
 
     it "must have a value" do
-      @event.value.must_be_kind_of(Integer)
+      expect(@event.value).must_be_kind_of(Integer)
     end
 
     it "must have string representations" do
       [:to_s, :pretty, :raw, :hex].each { |meth|
-        @event.send(meth).must_be_instance_of(String)
+        expect(@event.send(meth)).must_be_instance_of(String)
       }
     end
   end
@@ -91,8 +91,8 @@ describe DeviceInput do
       DeviceInput.read_loop(@io) { |event|
         events << event
       }
-      events.wont_be_empty
-      events.first.must_be_instance_of(E)
+      expect(events).wont_be_empty
+      expect(events.first).must_be_instance_of(E)
     end
   end
 end
