95a2754ce689538733038107abc297b2b53e670b
Rick Hull <rick.hull@gmail.com>
2023-01-31 12:35:47 -0500

    reformat; don't use nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,17 +1,22 @@
 {
   description = "Provides a shell with ASDF version manager available";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils}:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.bashInteractive ];
-        buildInputs = [
-	  pkgs.asdf-vm
-        ];
-      };
-    });
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils}:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.bashInteractive ];
+            buildInputs = [
+	      pkgs.asdf-vm
+            ];
+          };
+        }
+      );
 }
